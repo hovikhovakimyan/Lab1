6d1c4af7b5de0559cb9b919e50d27fcc9b86a5f2
unknown <hovo27@gmail.com>
2020-02-29 02:40:56 -0800

    Second Commit

--- project2.c
+++ project2.c
@@ -2,17 +2,25 @@
 #include<time.h>
 #include<sys/types.h>
 #include<unistd.h>
-#define _XOPEN_SOURCE 600
-
+#include<sys/wait.h>
+#include <sys/times.h>
 
 int main(){
-    time_t seconds;
-    seconds = time(NULL);
+    int firstpid = getpid();
+    struct tms *tms;
+    times(tms);
+    printf("START: %ld\n", time(NULL));
     int p;
     p=fork();
-    printf("START: %ld\n", seconds);
+    if(getpid()==firstpid){
+        printf("PPID: %d, PID: %d, CPID: %d, RETVAL: %d\n", getppid(), getpid(), p, wait(NULL));
+        printf("USER: %ld, SYS: %ld\n",tms->tms_utime, tms->tms_stime);
+        printf("CUSER: %ld, CSYS: %ld\n",tms->tms_cutime, tms->tms_cstime);
+        printf("Exit: %ld",time(NULL));
+    }
+    else{
     printf("PPID: %d, PID: %d\n", getppid(), getpid());
-
+    }
     return 1;
 }
 
