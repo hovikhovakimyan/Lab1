df3be690f0dec49a9c237e577c0066d5ff203558
unknown <hovo27@gmail.com>
2020-02-29 03:13:21 -0800

    Third Commit

--- project2.c
+++ project2.c
@@ -7,18 +7,20 @@
 
 int main(){
     int firstpid = getpid();
+    int status;
     struct tms *tms;
     times(tms);
     printf("START: %ld\n", time(NULL));
     int p;
     p=fork();
-    if(getpid()==firstpid){
-        printf("PPID: %d, PID: %d, CPID: %d, RETVAL: %d\n", getppid(), getpid(), p, wait(NULL));
+    if(p>0){
+        wait(&status);
+        printf("PPID: %d, PID: %d, CPID: %d, RETVAL: %d\n", getppid(), getpid(), p, WIFEXITED(status));
         printf("USER: %ld, SYS: %ld\n",tms->tms_utime, tms->tms_stime);
         printf("CUSER: %ld, CSYS: %ld\n",tms->tms_cutime, tms->tms_cstime);
         printf("Exit: %ld",time(NULL));
     }
-    else{
+    if(p==0){
     printf("PPID: %d, PID: %d\n", getppid(), getpid());
     }
     return 1;
